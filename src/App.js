c785f0851e6135c624e4a8d8795caef5b0342874
Godspower Idiakose <idiakeg@gmail.com>
2022-08-27 02:46:04 +0100

    cleared the default create-react-app files and included a .env file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const dummy = process.env.REACT_APP_DUMMY_TEXT;
+	const handleClick = () => {
+		console.log("button was clicked");
+		console.log(dummy);
+	};
+	return (
+		<div className="container">
+			<button onClick={handleClick}>click</button>
+		</div>
+	);
 }
 
 export default App;
